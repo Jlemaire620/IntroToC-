9bf54d13877d4adc38ea973bf250f424830745c5
Holiday Course <holiday@ad.aie.edu>
2018-08-13 16:35:05 -0500

    Add project files.

---
+++ Intro To C++/Source.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Jordan Lemaire" << std::endl;
+	std::cout << "Apple" << std::endl;
+	std::cout << "Orange" << std::endl;
+	std::cout << "1 Lion Sleeps Tonight" << std::endl;
+	std::cout << "2 Firework" << std::endl;
+	std::cout << "3 Happy" << std::endl;
+	std::cout << "4 I hate u I love u" << std::endl;
+	std::cout << "5 All about that base" << std::endl; 
+	std::cout << "6 Shape of You" << std::endl;
+	std::cout << "7 Despacito" << std::endl;
+	std::cout << "8 Thriller" << std::endl;
+	std::cout << "9 Stairway to Heaven" << std::endl;
+	std::cout << "10 Unforgetable" << std::endl; 
+
+	std::cout << '0' << std::endl;
+	std::cout << '1' << std::endl;
+	std::cout << '2' << std::endl;
+	std::cout << '3' << std::endl;
+	std::cout << '4' << std::endl;
+	std::cout << '5' << std::endl;
+
+	std::cout << "5\n4\n3\n2\n1\n0" << std::endl;
+	system("pause");
+}
